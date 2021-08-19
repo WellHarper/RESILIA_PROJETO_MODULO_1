867cc65bfc69ff287fef38070626627b474fac1a
WellHarper <wellersonramos360@gmail.com>
2021-08-18 23:30:35 -0300

    subindo big smoke fases completa

--- js/QuestionSmoke.js
+++ js/QuestionSmoke.js
@@ -87,3 +87,60 @@ function Question1() {
           
            
         }
+
+        function Question3() {
+
+
+            do{
+            
+            var resposta = prompt(`Em uma igreja havia 4 velas. Entraram 2 ladrões e cada um levou uma vela. 
+            Quantas velas ficaram?:
+            1 - 1 Vela
+            2 - 2 Velas
+            3 - 4 Velas
+            4 - 6 Velas
+            ` )
+            } while(resposta.length == 0)
+            
+            resposta = Number(resposta)
+                        
+            if(typeof resposta == 'number') {
+                // console.log(typeof resposta)
+                if(resposta == 1) {
+                var resposta = document.querySelector('#answer').innerText = 'JA era, ele nos viu!! NAAAAAAO!!'
+                    setTimeout(function() {
+                        return  window.location.href = "luisegidioGAMEOVER.html";
+                    }, 2000)
+                
+                }
+                else if(resposta == 2) {
+                    var resposta = document.querySelector('#answer').innerText = 'Como sao ladroes, podem muito bem ter levado embora as velas. Conseguimos!!!'
+                    var button = document.querySelector('.playButton').style.display = 'none'
+                    setTimeout(function() {
+                        return  window.location.href = "luisegidioFIM.html";
+                    }, 5000)
+                
+                }
+                else if(resposta == 3) {
+                    var resposta = document.querySelector('#answer').innerText = 'Parabens!! Estamos mortos'
+                    var button = document.querySelector('.playButton').style.display = 'none'
+                    setTimeout(function() {
+                        return  window.location.href = "luisegidioGAMEOVER.html";
+                    }, 2000)
+                
+                }
+                else if (resposta == 4) {
+                    var resposta = document.querySelector('#answer').innerText = 'O levarmos pode gerar ambiguidade, portanto ao levar podem ter levado duas velas para a igreja!! Conseguimos'
+                    var button = document.querySelector('.playButton').style.display = 'none'
+                    setTimeout(function() {
+                        return  window.location.href = "luisegidioFIM.html";
+                    }, 5000)
+                
+                }
+    
+            
+            }
+    
+              
+               
+            }
